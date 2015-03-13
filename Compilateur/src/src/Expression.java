33ff767f7ede199cc3405924ed1b31cd6f77a7b0
simorachid <ra.simo@hotmail.fr>
2015-03-13 16:52:32 +0100

    ajout de la classe expression et ces fonction dans Yaka.jj

--- Compilateur/src/src/Expression.java
+++ Compilateur/src/src/Expression.java
@@ -1,50 +1,93 @@
-package src;
+import java.io.InputStream;
+import java.util.Stack;
 
 
-public class Expression {
+public class Expression{
 
-/**
-    Vérifie le type des opérandes d'une expression
-    @param type1 Type du premier opérande 
-    @param type2 Type du deuxième opérande
-    @param op Opérateur (1:{+,-,*,/}  2:{<,>,<=,>=}  3:{=,!=} 4:{&&,||}  
-    @return Type du résultat de l'opération (rend "erreur" si type incorrect)
-*/
-	public String controleType(String type1, String type2, int op){
-		String resul = "";
-		switch(op){
-			case 1 :
-				if(type1.equals("entier") && type2.equals("entier")){
-					resul = "entier";
-				}else resul = "erreur";
-				break;
-			
-			case 2 :
-				if(type1.equals("entier") && type2.equals("entier")){
-					resul = "booleen";
-				}else resul = "erreur";
-				break;
-				
-			case 3 :
-				if(type1.equals("erreur") || type2.equals("erreur")){
-					resul = "erreur";
-				}else resul = "booleen";
-				break;
-				
-			case 4 :
-				if(type1.equals("booleen") && type2.equals("booleen")){
-					resul = "booleen";
-				}else resul = "erreur";
-				break;
-			
-			default :
-				resul = "erreur";
-		
-		}
-		
-		return resul;
+	public enum tip {
+		ENTIER,
+		BOOL,
+		ERREUR;	
+	};
+
+	public enum op {
+		PLUS,
+		MOINS,
+		DIV,
+		MUL,
+		DIFF,
+		EGAL,
+		INF,
+		INFEGAL,
+		SUP,
+		SUPEGAL,
+		OU,
+		ET,
+		NON;	  
+	};
+
+	Stack<tip> type = new Stack();
+	Stack<op> oper = new Stack();
+
+	public void saveType(tip t){
+		type.push(t);
 	}
-	
 
+	public void saveOp(op p){
+		oper.push(p);
+	}
+
+	public void testType2Argv(){
+		
+		if(!type.isEmpty() && !oper.isEmpty()){
+
+			tip tp = type.pop();
+			op o = oper.pop();
+			
+			if(type.pop()!=tp)
+			{
+				type.push(tip.ERREUR);
+			}
+			else
+			{
+				switch(o){
+
+				case EGAL:
+				case INF:
+				case SUP:
+				case SUPEGAL:
+				case INFEGAL:
+				case DIFF:
+					type.push(tip.BOOL);
+				default:
+					type.push(tip.ENTIER);
+				}
+			}
+		}
+			
+	}
+
+	public void testType1Argv(){
+		
+		if(!type.isEmpty() && !oper.isEmpty()){
+
+			if(type.peek() == tip.ENTIER && oper.peek()==op.MOINS)
+			{
+				type.pop();
+				oper.pop();
+				type.push(tip.ENTIER);
+			}
+			else if(type.peek() == tip.BOOL && oper.peek()==op.NON)
+			{	
+				type.pop();
+				oper.pop();
+				type.push(tip.BOOL);
+			}
+			else{
+
+				type.push(tip.ERREUR);
+			}
+		}
+	}
 
 }
